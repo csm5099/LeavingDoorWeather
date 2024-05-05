6f7539d185766ab4a18fbf73df51adfd502510ab
Sungmin <csm5099@gmail.com>
2024-05-06 01:28:17 +0900

    파일을 분리하여 loop()구간에서 적은 코드로 기능할 수 있게한다.

--- porject_weather.ino
+++ porject_weather.ino
@@ -1,5 +1,7 @@
 #include "getRainAPIUrl.h"
 #include "apiRequest.h"
+#include "getRainData.h"
+#include "isRain.h"
 
 String RainAPIUrl = getRainAPIUrl();
 
@@ -9,8 +11,11 @@ void setup() {
 }
 
 void loop() {
-  String RainAPIUrl = getRainAPIUrl();
-  apiRequest(RainAPIUrl);
-  
+  String rainAPIUrl = getRainAPIUrl();
+  String rainJSON = apiRequest(rainAPIUrl);
+  String rainCheck = getRainData(rainJSON);
+  boolean takeUmbrella = isRain(rainCheck);
+  if(takeUmbrella == true) Serial.println("우산을 챙기세요");
+  else Serial.println("비소식이 없어요");
   delay(5000);
 }
