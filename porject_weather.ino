75f5247381fb6bdbe5bc4096260d4d154a56544e
Sungmin <csm5099@gmail.com>
2024-05-06 19:24:14 +0900

    비 정보 함수들을 하나의 함수로 묶음.

--- porject_weather.ino
+++ porject_weather.ino
@@ -11,11 +11,24 @@ void setup() {
 }
 
 void loop() {
+  rainFunc();
+  delay(5000);
+}
+
+void rainFunc(){
+  // api url 생성
   String rainAPIUrl = getRainAPIUrl();
+
+  // api 요청
   String rainJSON = apiRequest(rainAPIUrl);
+  
+  // api 데이터 수집
   String rainCheck = getRainData(rainJSON);
+  
+  // 장치 출력
   boolean takeUmbrella = isRain(rainCheck);
+  
+  // 
   if(takeUmbrella == true) Serial.println("우산을 챙기세요");
   else Serial.println("비소식이 없어요");
-  delay(5000);
 }
