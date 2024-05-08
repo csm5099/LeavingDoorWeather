e980d8b80712973f862178a32724141363765afd
Sungmin <csm5099@gmail.com>
2024-05-08 14:50:25 +0900

    자외선 차단제 바르기 판단하는 기능 추가

--- porject_weather.ino
+++ porject_weather.ino
@@ -1,8 +1,7 @@
-#include "getRainAPIUrl.h"
 #include "apiRequest.h"
+#include "getWeatherAPIUrl.h"
 #include "isRain.h"
-
-String RainAPIUrl = getRainAPIUrl();
+#include "isBurn.h"
 
 void setup() {
   Serial.begin(9600);
@@ -11,16 +10,16 @@ void setup() {
 
 void loop() {
   rainFunc();
+  uvFunc();
   delay(5000);
 }
 
 void rainFunc(){
   // api url 생성
-  String rainAPIUrl = getRainAPIUrl();
+  String rainAPIUrl = getWeatherAPIUrl();
 
   // api 요청 후 데이터 수집
   String rainJSON = apiRequest(rainAPIUrl);
-  Serial.println(rainAPIUrl);
   
   //데이터 가공, 비와 눈 소식 필터링
   boolean takeUmbrella = isRain(rainJSON);
@@ -28,4 +27,19 @@ void rainFunc(){
   // 출력 장치 출력
   if(takeUmbrella == true) Serial.println("우산을 챙기세요");
   else Serial.println("비소식이 없어요");
+}
+
+void uvFunc(){
+  // api url 생성
+  String uvAPIUrl = getWeatherAPIUrl();
+
+  // api 요청 후 데이터 수집
+  String uvJSON = apiRequest(uvAPIUrl);
+  
+  //데이터 가공 자외선 수치 필터링
+  boolean takeSunblock = isBurn(uvJSON);
+
+  //출력 장치 출력
+  if(takeSunblock == true) Serial.println("Put sunblock on!");
+  else Serial.print("It's fine to skip putting sunblock on.");
 }
